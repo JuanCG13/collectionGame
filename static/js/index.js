2fc63297148c43d54d796d390501b71965d20c85
goresetpy2 <goresetpy2@protonmail.com>
2021-06-12 22:50:51 -0400

    add changes for random images

--- static/js/index.js
+++ static/js/index.js
@@ -2,12 +2,52 @@ var vm = new Vue({
     el: '#app',
 
     data: {
-        showImage: false
+        showImage: false,
+        urlImage: [
+            {
+                url: 'static/assets/collectables/messi.jpg',
+                probability: 90
+            },
+            {
+                url: 'static/assets/collectables/gokuUltra.jpg',
+                probability: 50
+            },
+            {
+                url: 'static/assets/collectables/narutoChico.jpg',
+                probability: 10
+            },
+            {
+                url: 'static/assets/collectables/rockLeeChico.png',
+                probability: 10
+            },
+            {
+                url: 'static/assets/collectables/sakuraChica.jpg',
+                probability: 10
+            },
+            {
+                url: 'static/assets/collectables/sasukeChico.jpeg',
+                probability: 10
+            },
+            {
+                url: 'static/assets/collectables/ronaldo.jpg',
+                probability: 10
+            },
+            {
+                url: 'static/assets/collectables/vegetaSSJBLUE.png',
+                probability: 10
+            }
+        ],
+        selectImage: ''
     },
 
     methods: {
         showimageClick() {
-            this.showImage = true
+            this.showImage = true ? this.showImage === false : false
+            const randomElement = this.urlImage[Math.floor(Math.random() * this.urlImage.length)];
+            this.selectImage = randomElement.url
+        },
+        reset() {
+            this.showImage = false
         }
     }
 })
